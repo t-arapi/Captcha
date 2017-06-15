90cd29883c266f6e99391738982365da8164effc
t-arapi <letrata@gmail.com>
2017-06-15 12:04:19 +0200

    Create conf.php

---
+++ config/conf.php
@@ -0,0 +1,27 @@
+<?php
+
+defined('CMS_TA') or die;
+
+return [
+	'db' => [
+		'driver' 	=> 'mysql',
+		'host' 		=> 'localhost',
+		'name' 		=> 'bbbbbb',
+		'user' 		=> 'root',
+		'psw' 		=> 'zaza',
+		'charset' 	=> 'utf8',
+		'collation' => 'utf8_unicode_ci',
+		'prefix' 	=> 'aa_'
+	],
+	'session' => [
+		'save' 			=> 'file',
+		'name' 			=> 'qepa',
+		'time' 			=> 600,
+		'regenerate' 	=> false,
+		'encode' 		=> true
+	],
+	'sito' => [
+		'debug' 	=> true,
+		'title' 	=> 'Web T-A'
+	]
+];
